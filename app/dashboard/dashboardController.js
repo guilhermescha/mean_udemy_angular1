ac54a9929798ee9144a5c636b3a30dc3ffc8e87a
guilhermescha <guilhermescha@live.com>
2017-04-06 22:26:38 -0300

    Parte responsável a exibir valores do Dashboard atualizada para seguir boas práticas do Angular (Encapsulamento das funções e uso do ControllerAs).

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -1,21 +1,21 @@
 (function() {
     angular.module('primeiraApp').controller('DashboardCtrl', [
-        '$scope',
         '$http',
         DashBoardController
     ])
 
-    function DashBoardController($scope, $http) {
-        $scope.getSummary = function() {
+    function DashBoardController($http) {
+        const vm = this
+        vm.getSummary = function() {
             const url = 'http://localhost:3003/api/billingSummary'
             $http.get(url).then(function(response) {
                 const { credit = 0, debt = 0 } = response.data
-                $scope.credit = credit
-                $scope.debt = debt
-                $scope.total = credit - debt
+                vm.credit = credit
+                vm.debt = debt
+                vm.total = credit - debt
             })
         }
 
-        $scope.getSummary()
+        vm.getSummary()
     }
 })()
