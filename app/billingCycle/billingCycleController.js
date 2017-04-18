6a986963442117e7074bb6a7b579798f84ec7ff5
guilhermescha <guilhermescha@live.com>
2017-04-17 23:24:49 -0300

    Implementada listagem, inclusão e validação de registros.

---
+++ app/billingCycle/billingCycleController.js
@@ -0,0 +1,32 @@
+(function() {
+    angular.module('primeiraApp').controller('BillingCycleCtrl', [
+        '$http',
+        'msgs',
+        'tabs',
+        BillingCycleController
+    ])
+
+    function BillingCycleController($http, msgs, tabs) {
+        const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = function() {
+            $http.get(url).then(function(response) {
+                vm.billingCycle = {}
+                vm.billingCycles = response.data
+                tabs.show(vm, { tabList: true, tabCreate: true })
+            })
+        }
+
+        vm.create = function() {
+            $http.post(url, vm.billingCycle).then(function(response) {
+                vm.refresh()
+                msgs.addSuccess('Operação realizada com sucesso!')
+            }).catch(function(response) {
+                msgs.addError(response.data.errors)
+            })
+        }
+
+        vm.refresh()
+    }
+})()
